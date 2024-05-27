f7241bc313984328791c31eb9efc298a990bee15
Karma Rangjung Dorji <02230096.cst@rub.edu.bt>
2024-05-27 15:04:31 +0600

    classwork.py

---
+++ classwork.py
@@ -0,0 +1,13 @@
+class dog:
+    def __init__(self, name, color, age):
+        self.name = name
+        self.color = color
+        self.age = age
+dawa = dog("dawa", "black", 23)
+nima = dog("nima", "white", 15)
+blacky = dog("blacky", "ash", 34)
+
+print(dawa.name, dawa.color, dawa.age)
+print(nima.name, nima.color, nima.age)
+print(blacky.name, blacky.color, blacky.age)
+
